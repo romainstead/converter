9100bb11c57bc94385d8447b70bc73978edfadc3
romainstead <lolkek12042004@gmail.com>
2025-03-26 20:25:56 +0700

    fuck no revert

--- src/services/converters_service.py
+++ src/services/converters_service.py
@@ -1,4 +1,3 @@
-import glob
 from datetime import datetime, UTC
 import logging
 import os
@@ -22,7 +21,6 @@ def search_for_currency(currency_list: list, key: str, need_currency: str):
 
 
 def convert_csv_files_utair(currency_type: str):
-    today = datetime.now(UTC)
     # Папки для работы
     parent_dir = Path(__file__).parent.parent.parent
     input_folder = parent_dir / 'need_convert'
@@ -75,103 +73,103 @@ def convert_csv_files_utair(currency_type: str):
     ]
 
     # Проходим по всем файлам в папке need_convert
-    pattern = f'prices_??_{today.year}-{today.month:02d}-{today.day:02d}*.csv'
-    files = list(glob.glob(str(input_folder / pattern)))
-    for filename in files:
-        # Читаем входной CSV файл
-        input_path = os.path.join(input_folder, filename)
-        df = pd.read_csv(input_path)
+    # pattern = f'prices_??_{today.year}-{today.month:02d}-{today.day:02d}*.csv'
+    for filename in os.listdir(input_folder):
+        if filename.endswith('.csv'):
+            # Читаем входной CSV файл
+            input_path = os.path.join(input_folder, filename)
+            df = pd.read_csv(input_path)
 
-        # Извлекаем код перевозчика из имени файла
-        cxr = filename.split('_')[1]
+            # Извлекаем код перевозчика из имени файла
+            cxr = filename.split('_')[1]
 
-        # Создаем новый DataFrame с нужными колонками
-        new_df = pd.DataFrame(columns=output_columns)
-        new_df['cxr'] = [cxr] * len(df) if not df.empty else [cxr]
+            # Создаем новый DataFrame с нужными колонками
+            new_df = pd.DataFrame(columns=output_columns)
+            new_df['cxr'] = [cxr] * len(df) if not df.empty else [cxr]
 
-        # Заполняем поля согласно ТЗ
-        new_df['orig atp'] = df.get('origin', '')
-        new_df['dest apt'] = df.get('destination', '')
-        new_df['first fcc'] = ''
-        new_df['first flt#'] = df.get('outbound_flight_no', '')
+            # Заполняем поля согласно ТЗ
+            new_df['orig atp'] = df.get('origin', '')
+            new_df['dest apt'] = df.get('destination', '')
+            new_df['first fcc'] = ''
+            new_df['first flt#'] = df.get('outbound_flight_no', '')
 
-        # Форматирование first flt date в DD.MM.YYYY HH:MM
-        first_flt_date = df.get('outbound_departure_date', '')
-        if not first_flt_date.empty:
-            new_df['first flt date'] = pd.to_datetime(first_flt_date, format='mixed', dayfirst=True).dt.strftime(
-                '%d.%m.%Y %H:%M')
-        else:
-            new_df['first flt date'] = ''
+            # Форматирование first flt date в DD.MM.YYYY HH:MM
+            first_flt_date = df.get('outbound_departure_date', '')
+            if not first_flt_date.empty:
+                new_df['first flt date'] = pd.to_datetime(first_flt_date, format='mixed', dayfirst=True).dt.strftime(
+                    '%d.%m.%Y %H:%M')
+            else:
+                new_df['first flt date'] = ''
 
-        # Добавляем данные в comp, если есть колонка service_class
-        new_df['comp'] = df.get('service_class', '')
+            # Добавляем данные в comp, если есть колонка service_class
+            new_df['comp'] = df.get('service_class', '')
 
-        # Учитываем оба варианта price_exc/price_ex и добавляем .0
-        if 'price_exc' in df.columns:
-            # Переводим столбец в числовой тип
-            numeric_amt = pd.to_numeric(df['price_exc'], errors='coerce')
+            # Учитываем оба варианта price_exc/price_ex и добавляем .0
+            if 'price_exc' in df.columns:
+                # Переводим столбец в числовой тип
+                numeric_amt = pd.to_numeric(df['price_exc'], errors='coerce')
+                # Делим на курс обмена
+                numeric_amt = numeric_amt / exchange_rate
+                # Переводим в строки
+                new_df['amt'] = numeric_amt.map(lambda x: f"{x:.1f}" if pd.notnull(x) else '')
+                new_df['inclusive'] = new_df['amt']
+
+            elif 'price_ex' in df.columns:
+                # Переводим столбец в числовой тип
+                numeric_amt = pd.to_numeric(df['price_ex'], errors='coerce')
+                # Делим на курс обмена
+                numeric_amt = numeric_amt / exchange_rate
+                # Переводим в строку
+                new_df['amt'] = numeric_amt.map(lambda x: f"{x:.1f}" if pd.notnull(x) else '')
+                new_df['inclusive'] = new_df['amt']
+            else:
+                new_df['amt'] = ''
+                new_df['inclusive'] = ''
+            # Переводим в числовой формат
+            numeric_tax = pd.to_numeric(df.get('tax', ''), errors='coerce')
             # Делим на курс обмена
-            numeric_amt = numeric_amt / exchange_rate
-            # Переводим в строки
-            new_df['amt'] = numeric_amt.map(lambda x: f"{x:.1f}" if pd.notnull(x) else '')
-            new_df['inclusive'] = new_df['amt']
-
-        elif 'price_ex' in df.columns:
-            # Переводим столбец в числовой тип
-            numeric_amt = pd.to_numeric(df['price_ex'], errors='coerce')
-            # Делим на курс обмена
-            numeric_amt = numeric_amt / exchange_rate
+            numeric_tax = numeric_tax / exchange_rate
             # Переводим в строку
-            new_df['amt'] = numeric_amt.map(lambda x: f"{x:.1f}" if pd.notnull(x) else '')
-            new_df['inclusive'] = new_df['amt']
-        else:
-            new_df['amt'] = ''
-            new_df['inclusive'] = ''
-        # Переводим в числовой формат
-        numeric_tax = pd.to_numeric(df.get('tax', ''), errors='coerce')
-        # Делим на курс обмена
-        numeric_tax = numeric_tax / exchange_rate
-        # Переводим в строку
-        new_df['tax'] = numeric_tax.map(lambda x: f"{x:.1f}" if pd.notnull(x) else '')
-        # Заменяем название курса на тот, в который конвертировали
-        new_df['cur'] = need_currency
-        new_df['fty'] = 'OW'
+            new_df['tax'] = numeric_tax.map(lambda x: f"{x:.1f}" if pd.notnull(x) else '')
+            # Заменяем название курса на тот, в который конвертировали
+            new_df['cur'] = need_currency
+            new_df['fty'] = 'OW'
 
-        # Форматирование shapshot в DD.MM.YYYY HH:MM
-        shapshot_raw = df['observation_date'] + ' ' + df['observation_time']
-        new_df['shapshot'] = pd.to_datetime(shapshot_raw, format='mixed', dayfirst=True).dt.strftime(
-            '%d.%m.%Y %H:%M')
+            # Форматирование shapshot в DD.MM.YYYY HH:MM
+            shapshot_raw = df['observation_date'] + ' ' + df['observation_time']
+            new_df['shapshot'] = pd.to_datetime(shapshot_raw, format='mixed', dayfirst=True).dt.strftime(
+                '%d.%m.%Y %H:%M')
 
-        new_df['out_booking_class'] = df.get('booking_class', '')
-        new_df['source'] = df.get('source', '')
+            new_df['out_booking_class'] = df.get('booking_class', '')
+            new_df['source'] = df.get('source', '')
 
-        # Оставляем пустыми поля, не указанные в явном маппинге ТЗ
-        new_df['last fcc'] = ''
-        new_df['last flt#'] = ''
-        new_df['last flt date'] = ''
-        new_df['season'] = ''
-        new_df['advpur'] = ''
-        new_df['minstay'] = ''
-        new_df['maxstay'] = ''
-        new_df['first ticket'] = ''
-        new_df['last ticket'] = ''
-        new_df['first travel'] = ''
-        new_df['last travel'] = ''
-        new_df['penalty'] = ''
-        new_df['type'] = ''
-        new_df['pos'] = ''
-        new_df['outbound_travel_stop_over'] = df.get('transfer_iata', '')
-        new_df['homebound_travel_stop_over'] = ''
-        new_df['home_booking_class'] = ''
+            # Оставляем пустыми поля, не указанные в явном маппинге ТЗ
+            new_df['last fcc'] = ''
+            new_df['last flt#'] = ''
+            new_df['last flt date'] = ''
+            new_df['season'] = ''
+            new_df['advpur'] = ''
+            new_df['minstay'] = ''
+            new_df['maxstay'] = ''
+            new_df['first ticket'] = ''
+            new_df['last ticket'] = ''
+            new_df['first travel'] = ''
+            new_df['last travel'] = ''
+            new_df['penalty'] = ''
+            new_df['type'] = ''
+            new_df['pos'] = ''
+            new_df['outbound_travel_stop_over'] = df.get('transfer_iata', '')
+            new_df['homebound_travel_stop_over'] = ''
+            new_df['home_booking_class'] = ''
 
-        # # Проверяем cxr перед сохранением
-        # logger.info(f"first 5 new_df['cxr'] перед сохранением: {new_df['cxr'].head().tolist()}")
+            # # Проверяем cxr перед сохранением
+            # logger.info(f"first 5 new_df['cxr'] перед сохранением: {new_df['cxr'].head().tolist()}")
 
-        # Сохраняем результат в выходной файл
-        output_path = os.path.join(output_folder, filename)
-        new_df.to_csv(output_path, index=False, encoding='utf-8')
+            # Сохраняем результат в выходной файл
+            output_path = os.path.join(output_folder, filename)
+            new_df.to_csv(output_path, index=False, encoding='utf-8')
 
-        logger.info(f"file is converted: {Path(filename).stem}")
+            logger.info(f"file is converted: {Path(filename).stem}")
 
     logger.info("file conversion is end")
 
